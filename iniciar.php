8b340c7aa53cc9f6fbcbbece9e86308edde2cc94
zSamUzs <samuelcoyflores@gmail.com>
2024-08-15 11:28:33 -0600

    Examen1

--- iniciar.php
+++ iniciar.php
@@ -6,9 +6,15 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="revisar.php" method="post">
+<div class="text-center">
+       <h1 class="h4 text-gray-900 mb-4">Bienvenido Viajero</h1>
+     </div>
+     <div class="text-center">
+     h2 class="<h2></h2> text-gray-900 mb-4">Bienvenido Viajero</h2>  <form action="revisar.php" method="post">
+     </div>
     <input type="text" name="nombre"><br><br>
-    <input type="password" name="pass"><br><br>
+    <input type="password" name="N"><br><br>
+    <input type="text" name="nombre">
     <button type="submit">Iniciar</button>
     </form>
 </body>
